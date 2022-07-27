d995f58dd644bd95607b9307105e5cb3ade12e32
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2022-07-27 22:31:07 +0900

    Update Testplugin.java

--- src/main/java/com/kinoko/testplugin/Testplugin.java
+++ src/main/java/com/kinoko/testplugin/Testplugin.java
@@ -1,11 +1,14 @@
 package com.kinoko.testplugin;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Testplugin extends JavaPlugin implements Listener {
@@ -13,7 +16,7 @@ public final class Testplugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getLogger().info("Hello World!");
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -25,15 +28,24 @@ public final class Testplugin extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (player.hasPlayedBefore()) {
-            event.setJoinMessage(player.getName() + ChatColor.GREEN + "がログインしました");
+            event.setJoinMessage(ChatColor.WHITE + "[" + ChatColor.GOLD + "MesiOka" + ChatColor.LIGHT_PURPLE + "Server" + ChatColor.WHITE + "]" + player.getName() + ChatColor.GREEN + "がログインしました");
         } else {
-            event.setJoinMessage(player.getName() + ChatColor.YELLOW + "が初ログインしました");
+            Inventory inv = player.getInventory();
+            //ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
+            //ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET);
+            //player.getInventory().setItem(0,sword);
+            //player.getInventory().setHelmet(helmet);
+            //player.updateIntentory();
+            inv.setItem(0, new ItemStack(Material.WOODEN_PICKAXE,1));
+            inv.setItem(1, new ItemStack(Material.WOODEN_AXE,1));
+            inv.setItem(2, new ItemStack(Material.WOODEN_SHOVEL,1));
+            event.setJoinMessage(ChatColor.WHITE + "[" + ChatColor.GOLD + "MesiOka" + ChatColor.LIGHT_PURPLE + "Server" + ChatColor.WHITE + "]" + player.getName() + ChatColor.YELLOW + "が初ログインしました");
         }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        event.setQuitMessage(player.getName() + ChatColor.GREEN + "がログアウトしました");
+        event.setQuitMessage(ChatColor.WHITE + "[" + ChatColor.GOLD + "MesiOka" + ChatColor.LIGHT_PURPLE + "Server" + ChatColor.WHITE + "]" + player.getName() + ChatColor.GREEN + "がログアウトしました");
     }
 }
